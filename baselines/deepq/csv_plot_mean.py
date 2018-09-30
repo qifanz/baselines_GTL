aefa3993c1c6fd547180253ba2d0304c574763c6
qifanz <qifan.zhang@insa-lyon.fr>
2018-09-30 17:00:50 +0200

    add comments

--- baselines/deepq/csv_plot_mean.py
+++ baselines/deepq/csv_plot_mean.py
@@ -4,22 +4,27 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
-
+# Read the eps_fraction_mean.csv which contains different combinations of eps and fraction
 log = [l.split("\n")[0].split(",") for l in open(os.path.join('./log', 'eps_fraction_mean.csv')).readlines()]
 log = log[1:]  # ignore the first line which is a string comment
 log = np.array(log)
+# Add one dimension to the array (which will contain the max_reward)
 log = np.hstack((log, np.zeros((log.shape[0], 1), dtype=np.float32)))
 
-
+# For each row (eps,fraction), read the corresponding csv file and add the max reward to the log array
+# TODO: replace max reward by mean max reward of different tentatives
 for i in range (0,len(log),1):
+    # Dir name is eps_fraction
     dir = os.path.join(log[i][0]+'_'+log[i][1])
     log2 = [l.split("\n")[0].split(",") for l in open(os.path.join('./log', dir, 'progress.csv')).readlines()]
     count = 0
+    # Look for the index of mean 100 episodes reward
     for j in log2[0]:
         if j == 'mean 100 episode reward':
             index_reward = count
         count=count+1
 
+    # Look for the max mean 100 episodes reward
     log2 = log2[1:]
     log2 = np.array(log2)
     mean_rew_100 = log2[:, index_reward].astype(np.float32)
@@ -27,8 +32,8 @@ for i in range (0,len(log),1):
     log[i][2]=max_reward
 
 
-
-# colum order q_max,q_min,episodes,mean 100 episode reward,steps,% time spent exploring
+# For each eps, plot the evolution of reward according to fraction
+# TODO: iterate the values contained in csv instead of manual inputs
 for eps in (0.01, 0.04, 0.07, 0.1, 0.15, 0.2, 0.25):
     fraction=[]
     mean=[]
@@ -45,6 +50,8 @@ for eps in (0.01, 0.04, 0.07, 0.1, 0.15, 0.2, 0.25):
     plt.scatter(fraction, mean)
     plt.savefig(os.path.join('./log', 'eps='+str(eps)+'.png'))
 
+# For each fraction, plot the evolution of reward according to eps
+# TODO: iterate the values contained in csv instead of manual inputs
 for fraction in (0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35):
     eps=[]
     mean=[]
