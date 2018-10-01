8d57f127d216bbf918daa1755b5a8cba02bbaaa6
Louis MORIN <louis@livemon.com>
2018-10-01 22:02:11 +0200

    Fixing typo

--- baselines/deepq/test_epsilon.sh
+++ baselines/deepq/test_epsilon.sh
@@ -4,7 +4,7 @@ trap exit SIGINT;
 
 for tentative in {1..10}
 do
-    for iters in 0.000001 0.00001 0.0001 0.001 0.01 0.1 0.5
+    for eps in 0.000001 0.00001 0.0001 0.001 0.01 0.1 0.5
     do
         python ./experiments/train_cartpole.py --prioritized_replay_eps=$eps --prioritized_replay=True --tentative=$tentative
     
