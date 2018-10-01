d1a3ea21f6caef20cd2ff84e9ea028f88439df2a
qifanz <qifan.zhang@insa-lyon.fr>
2018-10-01 18:17:49 +0200

    adapt to number of tentatives

--- baselines/deepq/create_log_dir.sh
+++ baselines/deepq/create_log_dir.sh
@@ -7,10 +7,8 @@ chmod 777 'log'
 
 for tentative in 1 2 3 4 5 6 7 8
 do
-    # for eps in 0.1 0.15 0.2 0.25
     for eps in 0.01 0.04 0.07 0.1 0.15 0.2 0.25
     do
-        #for fraction in 0.05 0.1 0.15 0.2
         for fraction in 0.05 0.1 0.15 0.2 0.25 0.3 0.35
         do
             target="./log/$eps"\_"$fraction/$tentative"
