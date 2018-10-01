c4a933b8f15fe8128ed6a0b6b4a91ab14771e3c2
Louis MORIN <louis@livemon.com>
2018-10-01 14:41:58 +0200

    Changing log paths

--- baselines/deepq/test_alpha.sh
+++ baselines/deepq/test_alpha.sh
@@ -2,14 +2,10 @@
 
 trap exit SIGINT;
 
-for tentative in 3 4 
+for tentative in {1..10}
 do
     for alpha in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
     do
-	 target="./log/True_$alpha_0.4_None_1e-6"
-            echo $target
-            mkdir -p $target
-            chmod 777 $target
         python ./experiments/train_cartpole.py --prioritized_replay_alpha=$alpha --prioritized_replay=True --tentative=$tentative
     
 done
--- baselines/deepq/test_prioritized.sh
+++ baselines/deepq/test_prioritized.sh
@@ -2,11 +2,7 @@
 
 trap exit SIGINT;
 
-for tentative in 1 2
+for tentative in {1..10}
 do
-   target="./log/True_0.6_0.4_None_1e-6"
-            echo $target
-            mkdir -p $target
-            chmod 777 $target
-    python ./experiments/train_cartpole.py --prioritized_replay=True --tentative=$tentative
+   python ./experiments/train_cartpole.py --prioritized_replay=True --tentative=$tentative
 done
