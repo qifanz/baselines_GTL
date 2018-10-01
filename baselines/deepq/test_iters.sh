df0be3ff42710ceb0979471120b3d57f6f46fbe1
Louis MORIN <louis@livemon.com>
2018-10-01 21:56:13 +0200

    Fixed tests

--- baselines/deepq/test_iters.sh
+++ baselines/deepq/test_iters.sh
@@ -4,7 +4,7 @@ trap exit SIGINT;
 
 for tentative in {1..10}
 do
-    for iters in 1 2 3 4 5
+    for iters in 100000 50000 10000 5000 
     do
         python ./experiments/train_cartpole.py --prioritized_replay_beta_iters=(100000/$iters) --prioritized_replay=True --tentative=$tentative
     
