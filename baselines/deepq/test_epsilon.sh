af218051c5f17f92947cec024bb9ee618583e280
Louis MORIN <louis@livemon.com>
2018-10-01 21:47:42 +0200

    Adding some tests

--- baselines/deepq/test_epsilon.sh
+++ baselines/deepq/test_epsilon.sh
@@ -6,7 +6,7 @@ for tentative in {1..10}
 do
     for iters in 1e-06 1e-05 1e-04 1e-03 1e-02 1e-01 0.5
     do
-        python ./experiments/train_cartpole.py --prioritized_replay_beta_iters=$eps --prioritized_replay=True --tentative=$tentative
+        python ./experiments/train_cartpole.py --prioritized_replay_eps=$eps --prioritized_replay=True --tentative=$tentative
     
 done
 done
