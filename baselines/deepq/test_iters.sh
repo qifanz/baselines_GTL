402bec883928a8518381f5d965abdcbca213453c
Louis MORIN <louis@livemon.com>
2018-10-01 21:59:28 +0200

    Fixing chmod issues

--- baselines/deepq/test_iters.sh
+++ baselines/deepq/test_iters.sh
@@ -6,7 +6,7 @@ for tentative in {1..10}
 do
     for iters in 100000 50000 10000 5000 
     do
-        python ./experiments/train_cartpole.py --prioritized_replay_beta_iters=(100000/$iters) --prioritized_replay=True --tentative=$tentative
+        python ./experiments/train_cartpole.py --prioritized_replay_beta_iters=$iters --prioritized_replay=True --tentative=$tentative
     
 done
 done
