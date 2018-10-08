3669d69e4e5555f6f3d704063ad33d20627e6d68
CADE <ecade@gtlpc106.georgiatech-metz.fr>
2018-10-08 14:22:12 +0200

    plot for buffer_size and learning_starts

--- baselines/deepq/csv_plot.py
+++ baselines/deepq/csv_plot.py
@@ -4,43 +4,92 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
+mean_results = np.zeros((36,3))
+
 list_dirs=os.walk('./log')
 for root, dirs, files in list_dirs:
+    path = 0
     for d in dirs:
-        log = [l.split("\n")[0].split(",") for l in open(os.path.join(root, d,'progress.csv')).readlines()]
+        depth = len(os.path.join(root,d).split(os.path.sep))
+        root_depth = len('./log'.split(os.path.sep))
+        
+        if depth == root_depth+1:
+	    mean_max_rew = np.array([[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]])#Associate max_rew from each tentative to its step    	    
 
-        count=0
-        for i in log[0]:
-            if i == 'mean 100 episode reward':
-                index_mean=count
-            elif i=="steps":
-                index_steps=count
-            count=count+1
-        print('index mean = ' + str(index_mean))
-        print('index steps = ' + str(index_steps))
-        log = log[1:]  # ignore the first line which is a string comment
-        # colum order q_max,q_min,episodes,mean 100 episode reward,steps,% time spent exploring
-        log = np.array(log)
-        print(len(log))
-        steps = log[:, index_steps].astype(np.float32)
-        mean_rew_100 = log[:, index_mean].astype(np.float32)
-        print(log)
+            for tentative in range(1,9,1):
+                log = [l.split("\n")[0].split(",") for l in open(os.path.join(root, d, str(tentative), 'progress.csv')).readlines()]
+                print("opened "+str(os.path.join(root, d, str(tentative), 'progress.csv')))
+                count=0
+                for i in log[0]:
+                    if i == 'mean 100 episode reward':
+                        index_mean=count
+                    elif i=="steps":
+                        index_steps=count
+                    count=count+1
+                
+                log = log[1:]  # ignore the first line which is a string comment
+                
+                log = np.array(log)
+                
+                #steps = log[:, index_steps].astype(np.float32)
+                mean_rew_100 = log[:, index_mean].astype(np.float32)
 
-        print("****************************steps********************************************")
+		max_rew = 0 #to retrieve the max reward
+		
+ 		
+		for j in mean_rew_100:
+		      if max_rew < mean_rew_100[j]:
+			  max_rew = mean_rew_100[j]
+			  index = j
+		
+		mean_max_rew[tentative][0] = max_rew
+		mean_max_rew[tentative][1] = index
 
-        print(steps)
-        print(np.max(steps))
 
-        print("**************************mean***************")
-        print(mean_rew_100)
-        print(np.max(mean_rew_100))
+	    
+	    #stores the mean of max_rew and steps from the 8 tentatives
+	    
+	    mean_results[path][0] = mean(mean_max_rew[:][0])# mean of the 8 max rewards
+	    mean_results[path][1] = mean(mean_max_rew[:][1])# mean of the 8 steps related to max_rew
+            mean_results[path][2] = str(d)# path leargningStarts_bufferSize
 
-        plt.figure()
-        plt.ylabel('Mean rew 100')
-        plt.xlabel('Steps')
-        plt.title('DQN - Mean reward for the least 100 episodes')
-        plt.axis([0, np.max(steps), 0, np.max(mean_rew_100)])
-        plt.scatter(steps, mean_rew_100)
-        plt.savefig(os.path.join(root, d, 'toto.png'))
+	    print(mean_results)
+ 
+	    path +=1
+
+
+for lngs in 100 500 800 1000 1200 1500
+    buffer_size = [25000, 35000, 50000, 65000, 75000, 100000]
+    meanL = []
+
+    for k in mean_results:
+	directory = mean_results[k][2]
+        if lngs == directory.split("_")[0]:
+	    meanL.append(mean_results[k][0]
+plt.figure()
+plt.ylabel('Mean rew 100')
+plt.xlabel('Buffer_size')
+plt.title('Learning_starts = '+str(lngs))
+plt.plot(buffer_size,meanL)
+plt.savefig(os.path.join(root, d,'Evolution mean reward - steps.png'))
+plt.close()
+
+
+
+for buffs in 25000 35000 50000 65000 75000 100000
+    leargning_starts = [100, 500, 800, 1000, 1200, 1500]
+    meanB = []
+
+    for k in mean_results:
+	directory = mean_results[k][2]
+        if buffs == directory.split("_")[1]:
+	    meanB.append(mean_results[k][0]
+plt.figure()
+plt.ylabel('Mean rew 100')
+plt.xlabel('Learning_starts')
+plt.title('Buffer_size = '+str(buffs))
+plt.plot(learning_starts,meanB)
+plt.savefig(os.path.join(root, d,'Evolution mean reward - steps.png'))
+plt.close()
 
 
