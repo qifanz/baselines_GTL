292d419a777110703fc65046cbc5fd378213af76
abenbihi <assia.benbihi@gmail.com>
2018-10-22 16:27:26 +0200

    modifi for mountaincar

--- baselines/deepq/test_eury_2.sh
+++ baselines/deepq/test_eury_2.sh
@@ -3,14 +3,15 @@
 
 trap exit SIGINT;
 
-for tentative in 3 4
+for tentative in 1
 do
   # for learning_starts in 100 500 800 1000 1200 1500
-  for lngs in 100 500 800 1000 1200 1500
+  for lngs in 1200 1500
   do
     for buffs in 25000 35000 50000 65000 75000 100000
     do
-        python ./experiments/train_cartpole.py --learning_starts=$lngs --buffer_size=$buffs --tentative=$tentative
+      #python ./experiments/train_cartpole.py --learning_starts=$lngs --buffer_size=$buffs --tentative=$tentative
+      python ./experiments/train_mountaincar.py --learning_starts=$lngs --buffer_size=$buffs --tentative=$tentative
     done
   done
 done
