17f424bed6cf8610e5408163877a1e380c437df6
qifanz <qifan.zhang@insa-lyon.fr>
2018-10-27 01:03:08 +0200

    update scripts

---
+++ baselines/deepq/test_mountaincar_5.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+trap exit SIGINT;
+
+for eps in 0.04 0.07 0.1 0.15 0.2 0.25
+do
+    for fraction in 0.04 0.07 0.1 0.15 0.2 0.25
+    do
+
+            for tentative in 5
+            do
+                python ./experiments/train_mountaincar.py --exploration_final_eps=$eps --exploration_fraction=$fraction --tentative=$tentative
+                python ./experiments/train_mountaincar.py --exploration_final_eps=$eps --exploration_fraction=$fraction --tentative=$tentative --param_noise=True
+
+            done
+    done
+done
