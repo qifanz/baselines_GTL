4111c9a3df214bdc1d4438c8371102101bfa5a60
qifanz <qifan.zhang@insa-lyon.fr>
2018-11-09 23:03:04 +0100

    update train breakout

--- baselines/deepq/experiments/train_pong.py
+++ baselines/deepq/experiments/train_pong.py
@@ -6,9 +6,10 @@ from baselines.common.atari_wrappers import make_atari
 
 def main():
     logger.configure()
-    env = make_atari('PongNoFrameskip-v4')
+    env = make_atari('BreakoutNoFrameskip-v4')
     env = bench.Monitor(env, logger.get_dir())
     env = deepq.wrap_atari_dqn(env)
+    logger.configure('../log/breakout', None)
 
     model = deepq.learn(
         env,
@@ -16,19 +17,19 @@ def main():
         convs=[(32, 8, 4), (64, 4, 2), (64, 3, 1)],
         hiddens=[256],
         dueling=True,
-        lr=1e-4,
-        total_timesteps=int(1e7),
-        buffer_size=10000,
+        lr=0.00025,
+        target_network_update_freq=10000,
+        total_timesteps=int(2e7),
+        buffer_size=1000000,
         exploration_fraction=0.1,
         exploration_final_eps=0.01,
         train_freq=4,
-        learning_starts=5000,
-        target_network_update_freq=1000,
+        learning_starts=50000,
         gamma=0.99,
         print_freq=10
     )
 
-    model.save('pong_model.pkl')
+    #model.save('pong_model.pkl')
     env.close()
 
 if __name__ == '__main__':
